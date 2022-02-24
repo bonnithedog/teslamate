a3f842efae9a7605ace390af7c515cbe5c55b397
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2022-02-24 17:32:23 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,15 +1,19 @@
 
 
 # Define Terraform provider
- terraform {
-#  required_version = ">= 0.14"
-   version         = "=2.97.0"
- }
+terraform {
+  required_providers {
+    azurerm = {
+      # source  = "hashicorp/azurerm"
+      version = "=2.97.0"
+    }
+  }
+}
 
 
  #Configure the Azure Provider
  provider "azurerm" {
    features {}
-   environment     = "public"
+  # environment     = "public"
 
  }
