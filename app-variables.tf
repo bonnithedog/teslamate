cb024c3a83e889aca80417c42a13bbbbaba41dee
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2022-02-24 17:46:38 +0100

    Create app-variables.tf

---
+++ app-variables.tf
@@ -0,0 +1,24 @@
+# company name 
+variable "company" {
+  type        = string
+  description = "The company name used to build resources"
+  default = "Bauer-TeslaMate"
+}
+# application name 
+variable "app_name" {
+  type        = string
+  description = "The application name used to build resources"
+  default = "TeslaMate"
+}
+# environment
+variable "environment" {
+  type        = string
+  description = "The environment to be built"
+  default = "prod"
+}
+# azure region
+variable "location" {
+  type        = string
+  description = "Azure region where the resources will be created"
+  default     = "north europe"
+}
