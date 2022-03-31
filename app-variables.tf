2d7f69231caf9dd42f2034abd7a2382d359d6ad5
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2022-04-01 00:59:33 +0200

    Update app-variables.tf

--- app-variables.tf
+++ app-variables.tf
@@ -20,5 +20,5 @@ variable "environment" {
 variable "location" {
   type        = string
   description = "Azure region where the resources will be created"
-  default     = "US Gov Texas"
+  default     = "Norway East"
 }
