733a32988b18abf2937df17b3dbe07ed1ef9ee1c
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2022-03-31 19:04:27 +0200

    Update app-variables.tf

--- app-variables.tf
+++ app-variables.tf
@@ -20,5 +20,5 @@ variable "environment" {
 variable "location" {
   type        = string
   description = "Azure region where the resources will be created"
-  default     = "North Europe"
+  default     = "West Europe"
 }
