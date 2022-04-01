de8431af9b245abd50064dca59856f8fb99457db
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2022-04-01 11:48:39 +0200

    Update linux-vm-main.tf

--- linux-vm-main.tf
+++ linux-vm-main.tf
@@ -136,6 +136,17 @@ resource "azurerm_network_interface" "web-linux-vm-nic" {
     environment = var.environment
   }
 }
+
+# Create azurerm_dns_a_record
+
+
+
+
+
+
+
+
+
 # Data template Bash bootstrapping file
 data "template_file" "linux-vm-cloud-init" {
   template = file("azure-user-data.sh")
