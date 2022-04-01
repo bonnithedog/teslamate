f7569ac33c139388dd8b592c6925455ee3c20806
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2022-04-01 11:20:19 +0200

    Update linux-vm-main.tf

--- linux-vm-main.tf
+++ linux-vm-main.tf
@@ -23,45 +23,45 @@ resource "azurerm_network_security_group" "web-linux-vm-nsg" {
   location            = azurerm_resource_group.network-rg.location
   resource_group_name = azurerm_resource_group.network-rg.name
    
-    security_rule {
-    name                       = "allow-ssh"
-    description                = "allow-ssh"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "Internet"
-    destination_address_prefix = "*" 
-  }
+ #  security_rule {
+ #  name                       = "allow-ssh"
+ #  description                = "allow-ssh"
+ #  priority                   = 100
+ #  direction                  = "Inbound"
+ #  access                     = "Allow"
+ #  protocol                   = "Tcp"
+ #  source_port_range          = "*"
+ #  destination_port_range     = "22"
+ #  source_address_prefix      = "Internet"
+ #  destination_address_prefix = "*" 
+ #}
 
-  
-      security_rule {
-    name                       = "allow-http"
-    description                = "allow-http"
-    priority                   = 108
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "Internet"
-    destination_address_prefix = "*" 
-  }
-  
-        security_rule {
-    name                       = "allow-https"
-    description                = "allow-https"
-    priority                   = 109
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "443"
-    source_address_prefix      = "Internet"
-    destination_address_prefix = "*" 
-  }
+ #
+ #    security_rule {
+ #  name                       = "allow-http"
+ #  description                = "allow-http"
+ #  priority                   = 108
+ #  direction                  = "Inbound"
+ #  access                     = "Allow"
+ #  protocol                   = "Tcp"
+ #  source_port_range          = "*"
+ #  destination_port_range     = "80"
+ #  source_address_prefix      = "Internet"
+ #  destination_address_prefix = "*" 
+ #}
+ #
+ #      security_rule {
+ #  name                       = "allow-https"
+ #  description                = "allow-https"
+ #  priority                   = 109
+ #  direction                  = "Inbound"
+ #  access                     = "Allow"
+ #  protocol                   = "Tcp"
+ #  source_port_range          = "*"
+ #  destination_port_range     = "443"
+ #  source_address_prefix      = "Internet"
+ #  destination_address_prefix = "*" 
+ #}
   
   
     security_rule {
@@ -77,18 +77,18 @@ resource "azurerm_network_security_group" "web-linux-vm-nsg" {
     destination_address_prefix = "*" 
   }
   
-      security_rule {
-    name                       = "allow-4000"
-    description                = "allow-4000"
-    priority                   = 120
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "4000"
-    source_address_prefix      = "Internet"
-    destination_address_prefix = "*" 
-  }
+ #     security_rule {
+ #   name                       = "allow-4000"
+ #   description                = "allow-4000"
+ #   priority                   = 120
+ #   direction                  = "Inbound"
+ #   access                     = "Allow"
+ #   protocol                   = "Tcp"
+ #   source_port_range          = "*"
+ #   destination_port_range     = "4000"
+ #   source_address_prefix      = "Internet"
+ #   destination_address_prefix = "*" 
+ # }
   
   
   
